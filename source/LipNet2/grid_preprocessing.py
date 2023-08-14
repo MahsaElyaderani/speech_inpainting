bcefdd05bb9b718271e4a5ad3961fda4c4291c91
mahsaElyaderani <kadkhodm@mcmaster.ca>
2023-08-14 10:43:14 -0400

    files are added

--- source/LipNet2/grid_preprocessing.py
+++
@@ -1,5 +0,0 @@
-from LipNet2.extract_mouth_batch import mouth_crop
-from LipNet2.GRID_refined_dataset import refine_grid
-
-mouth_crop()
-refine_grid()
--- source/PSNR.py
+++ source/PSNR.py
@@ -2,8 +2,11 @@ import numpy as np
 
 def PSNR(original, compressed):
     mse = np.mean((original - compressed) ** 2)
+    #l1 = np.mean(np.abs((original-compressed)*mask))
     if (mse == 0):  # MSE is zero means no noise is present in the signal, therefore PSNR have no importance.
         return 100
-    max_pixel = 255.0
+    max_pixel = 1.0
     psnr = 20 * np.log10(max_pixel / np.sqrt(mse))
+
     return psnr, mse
+
